b27f4a316fdd9dee25145219ac2a7124952907f4
Hieu Nguyen <axelnguyen0701@gmail.com>
2023-02-03 14:16:45 -0700

    Add department mock data

---
+++ CollegeRegistrationApp/CollegeRegistrationApp/SQL/Department.sql
@@ -0,0 +1,17 @@
+Use CollegeRegistration
+
+SET IDENTITY_INSERT Department ON
+
+
+insert into Department (Dept_ID, Dept_Name, Instructor_ID) values (1, 'Computer Science', '139');
+insert into Department (Dept_ID, Dept_Name, Instructor_ID) values (2, 'Accounting', '155');
+insert into Department (Dept_ID, Dept_Name, Instructor_ID) values (3, 'Engineering', '56');
+insert into Department (Dept_ID, Dept_Name, Instructor_ID) values (4, 'Mathematics', '55');
+insert into Department (Dept_ID, Dept_Name, Instructor_ID) values (5, 'English', '140');
+insert into Department (Dept_ID, Dept_Name, Instructor_ID) values (6, 'Biology', '193');
+insert into Department (Dept_ID, Dept_Name, Instructor_ID) values (7, 'Chemistry', '161');
+insert into Department (Dept_ID, Dept_Name, Instructor_ID) values (8, 'Physics', '32');
+insert into Department (Dept_ID, Dept_Name, Instructor_ID) values (9, 'Business', '164');
+insert into Department (Dept_ID, Dept_Name, Instructor_ID) values (10, 'Human Resources', '194');
+
+SET IDENTITY_INSERT Department OFF
