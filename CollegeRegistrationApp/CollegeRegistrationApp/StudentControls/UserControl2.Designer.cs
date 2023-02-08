7fa932936b6686a2fab5312069a91aaf9260ae08
Omar Ammouneh <ammouneho@mymacewan.ca>
2023-02-08 12:11:04 -0700

    More updates

--- CollegeRegistrationApp/CollegeRegistrationApp/StudentControls/UserControl2.Designer.cs
+++ CollegeRegistrationApp/CollegeRegistrationApp/StudentControls/UserControl2.Designer.cs
@@ -28,44 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listView1 = new System.Windows.Forms.ListView();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // listView1
-            // 
-            this.listView1.Location = new System.Drawing.Point(86, 130);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(794, 335);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(276, 44);
+            this.label1.Location = new System.Drawing.Point(289, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(395, 35);
             this.label1.TabIndex = 1;
             this.label1.Text = "Where enrolled classes are viewed";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(79, 304);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(773, 243);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(79, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Currently Enrolled";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(79, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cart";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(79, 93);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 29;
+            this.dataGridView2.Size = new System.Drawing.Size(773, 117);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(647, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Enroll";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(758, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Empty";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // UserControl2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listView1);
             this.Name = "UserControl2";
             this.Size = new System.Drawing.Size(972, 737);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private ListView listView1;
         private Label label1;
+        private DataGridView dataGridView1;
+        private Label label2;
+        private Label label3;
+        private DataGridView dataGridView2;
+        private Button button1;
+        private Button button2;
     }
 }
--- CollegeRegistrationApp/CollegeRegistrationApp/StudentControls/UserControl3.cs
+++ CollegeRegistrationApp/CollegeRegistrationApp/StudentControls/UserControl3.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace CollegeRegistrationApp.StudentControls
 {
@@ -15,6 +18,66 @@ namespace CollegeRegistrationApp.StudentControls
         public UserControl3()
         {
             InitializeComponent();
+            
         }
+
+        private void UserControl3_Load(object sender, EventArgs e)
+        {
+            Load_TermsMenuItems();
+        }
+
+
+
+        private void Load_TermsMenuItems()
+        {
+           
+            foreach(String items in Get_Terms_For_Menu())
+            {
+                ToolStripMenuItem item = new ToolStripMenuItem(items);
+                
+                toolStripMenuItem1.DropDownItems.Add(item);
+
+                item.Click += new EventHandler(Term_Click);
+            }
+            
+        }
+
+        private void Term_Click(object sender, EventArgs e)
+        {
+            // where u can show the different classes for the specific term
+
+            ToolStripMenuItem item = sender as ToolStripMenuItem;
+            // if item.Text = "Fall" or "Winter"
+        }
+
+        private List<String> Get_Terms_For_Menu()
+        {
+            int currentYear = DateTime.Now.Year;
+            int nextYear = currentYear + 1;
+
+            List<String> terms = new List<String>();
+
+            if (DateTime.Now.Month >= 1 && DateTime.Now.Month <= 4)
+            {
+                terms.Add("Fall " + currentYear);
+                terms.Add("Winter " + nextYear);
+            }
+            else if (DateTime.Now.Month >= 9 && DateTime.Now.Month <= 12)
+            {
+                terms.Add("Winter " + nextYear);
+                terms.Add("Fall " + nextYear);
+            }
+
+            return terms;
+
+        }
+
+       
     }
 }
+
+    
+
+
+
+
