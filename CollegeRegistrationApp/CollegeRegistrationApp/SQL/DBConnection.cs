bf302f42c911e3098838bce96712a32e9830efaf
sniezekk <sniezekk@mymacewan.ca>
2023-02-11 12:52:38 -0700

    Ported DBConnection from 291 and removed unused files

---
+++ CollegeRegistrationApp/CollegeRegistrationApp/SQL/DBConnection.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CollegeRegistrationApp.SQL
+{
+    public class DBConnection
+    {
+        readonly String connectionString = "Server = localhost; Database = Movies; Trusted_Connection = yes;";
+        SqlConnection connection;
+
+        public DBConnection()
+        {
+            // Constructor
+            connection = new SqlConnection(connectionString);
+        }
+        ~DBConnection()
+        {
+            // Destructor
+            connection.Close();
+        }
+
+        public void OpenConnection()
+        {
+            connection.Open();
+        }
+
+        //used to close the connection from other modules
+        public void CloseConnection()
+        {
+            connection.Close();
+        }
+
+
+        //execute a query from a string NOTE: Close the reader when done reading, then call CloseConnection()
+        public SqlDataReader? GetDataReader(String query) {
+            SqlDataReader? reader = null;
+            try
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                reader = command.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+             return reader;
+        }
+
+        // returns rows affect, uses a query string of plain sql
+        public int ExecuteMutation(String mutation)
+        {
+            int rowsAffected = 0;
+            try
+            {
+                SqlCommand command = new SqlCommand(mutation, connection);
+                rowsAffected =  command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.ToString());
+            }
+            return rowsAffected;
+        }
+
+        // returns the insertion id
+
+    }
+}
