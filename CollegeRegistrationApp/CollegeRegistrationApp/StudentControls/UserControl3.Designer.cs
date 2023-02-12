e4811413f5174e7d5ec691d2372916c2484fedb5
icynightcat <khana49@mymacewan.ca>
2023-02-11 22:23:37 -0700

    got the datagrid for classes working, will continue making this form perfect

--- CollegeRegistrationApp/CollegeRegistrationApp/StudentControls/UserControl3.Designer.cs
+++ CollegeRegistrationApp/CollegeRegistrationApp/StudentControls/UserControl3.Designer.cs
@@ -31,20 +31,30 @@
             this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.classScheduleTerm = new System.Windows.Forms.DataGridView();
+            this.DeptName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CourseName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sectionID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.days = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StartT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EndT = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Department = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classScheduleTerm)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(405, 29);
+            this.label1.Location = new System.Drawing.Point(506, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(152, 35);
+            this.label1.Size = new System.Drawing.Size(181, 41);
             this.label1.TabIndex = 0;
             this.label1.Text = "Class Search";
             // 
@@ -55,7 +65,8 @@
             this.toolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(972, 36);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1215, 40);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -64,51 +75,133 @@
             this.toolStripMenuItem1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.toolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(68, 32);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(83, 36);
             this.toolStripMenuItem1.Text = "Term";
             this.toolStripMenuItem1.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
             // 
-            // dataGridView1
+            // classScheduleTerm
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(74, 156);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(807, 371);
-            this.dataGridView1.TabIndex = 2;
+            this.classScheduleTerm.AllowUserToAddRows = false;
+            this.classScheduleTerm.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.classScheduleTerm.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DeptName,
+            this.CourseName,
+            this.sectionID,
+            this.days,
+            this.StartT,
+            this.EndT});
+            this.classScheduleTerm.Location = new System.Drawing.Point(92, 195);
+            this.classScheduleTerm.Margin = new System.Windows.Forms.Padding(4);
+            this.classScheduleTerm.Name = "classScheduleTerm";
+            this.classScheduleTerm.RowHeadersWidth = 51;
+            this.classScheduleTerm.RowTemplate.Height = 29;
+            this.classScheduleTerm.Size = new System.Drawing.Size(1009, 464);
+            this.classScheduleTerm.TabIndex = 2;
+            // 
+            // DeptName
+            // 
+            this.DeptName.HeaderText = "Department";
+            this.DeptName.MinimumWidth = 10;
+            this.DeptName.Name = "DeptName";
+            this.DeptName.Width = 150;
+            // 
+            // CourseName
+            // 
+            this.CourseName.HeaderText = "Course Name";
+            this.CourseName.MinimumWidth = 10;
+            this.CourseName.Name = "CourseName";
+            this.CourseName.Width = 208;
+            // 
+            // sectionID
+            // 
+            this.sectionID.HeaderText = "Section Number";
+            this.sectionID.MinimumWidth = 10;
+            this.sectionID.Name = "sectionID";
+            this.sectionID.Width = 150;
+            // 
+            // days
+            // 
+            this.days.HeaderText = "Days";
+            this.days.MinimumWidth = 10;
+            this.days.Name = "days";
+            this.days.Width = 150;
+            // 
+            // StartT
+            // 
+            this.StartT.HeaderText = "Start Time";
+            this.StartT.MinimumWidth = 10;
+            this.StartT.Name = "StartT";
+            this.StartT.Width = 150;
+            // 
+            // EndT
+            // 
+            this.EndT.HeaderText = "End Time";
+            this.EndT.MinimumWidth = 10;
+            this.EndT.Name = "EndT";
+            this.EndT.Width = 150;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(266, 81);
+            this.label2.Location = new System.Drawing.Point(332, 101);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.Size = new System.Drawing.Size(64, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "Search";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(338, 78);
+            this.textBox1.Location = new System.Drawing.Point(422, 98);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(278, 27);
+            this.textBox1.Size = new System.Drawing.Size(346, 31);
             this.textBox1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(137, 49);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(124, 33);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.Term_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(16, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 28);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Select Term:";
+            // 
+            // Department
+            // 
+            this.Department.HeaderText = "deptName";
+            this.Department.MinimumWidth = 8;
+            this.Department.Name = "Department";
+            this.Department.Width = 150;
             // 
             // UserControl3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.classScheduleTerm);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "UserControl3";
-            this.Size = new System.Drawing.Size(972, 737);
+            this.Size = new System.Drawing.Size(1215, 921);
             this.Load += new System.EventHandler(this.UserControl3_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classScheduleTerm)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,8 +212,17 @@
         private Label label1;
         private MenuStrip menuStrip1;
         private ToolStripMenuItem toolStripMenuItem1;
-        private DataGridView dataGridView1;
+        private DataGridView classScheduleTerm;
         private Label label2;
         private TextBox textBox1;
+        private ComboBox comboBox1;
+        private Label label3;
+        private DataGridViewTextBoxColumn DeptName;
+        private DataGridViewTextBoxColumn CourseName;
+        private DataGridViewTextBoxColumn sectionID;
+        private DataGridViewTextBoxColumn days;
+        private DataGridViewTextBoxColumn StartT;
+        private DataGridViewTextBoxColumn EndT;
+        private DataGridViewTextBoxColumn Department;
     }
 }
--- CollegeRegistrationApp/CollegeRegistrationApp/StudentControls/UserControl3.cs
+++ CollegeRegistrationApp/CollegeRegistrationApp/StudentControls/UserControl3.cs
@@ -10,15 +10,21 @@ using System.Windows.Forms;
 using static System.Net.Mime.MediaTypeNames;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Data.SqlClient;
+using CollegeRegistrationApp.SQL;
+using System.CodeDom.Compiler;
 
 namespace CollegeRegistrationApp.StudentControls
 {
     public partial class UserControl3 : UserControl
     {
-        public UserControl3()
+        private string student_id = "";
+        private DBConnection connection;
+        public UserControl3(string input, DBConnection input_connection)
         {
             InitializeComponent();
-            
+            connection = input_connection;
+            student_id = input;
         }
 
         private void UserControl3_Load(object sender, EventArgs e)
@@ -26,28 +32,17 @@ namespace CollegeRegistrationApp.StudentControls
             Load_TermsMenuItems();
         }
 
-
-
         private void Load_TermsMenuItems()
         {
-           
-            foreach(String items in Get_Terms_For_Menu())
+
+            foreach (String items in Get_Terms_For_Menu())
             {
                 ToolStripMenuItem item = new ToolStripMenuItem(items);
-                
-                toolStripMenuItem1.DropDownItems.Add(item);
 
-                item.Click += new EventHandler(Term_Click);
+                comboBox1.Items.Add(items);
+
             }
-            
-        }
 
-        private void Term_Click(object sender, EventArgs e)
-        {
-            // where u can show the different classes for the specific term
-
-            ToolStripMenuItem item = sender as ToolStripMenuItem;
-            // if item.Text = "Fall" or "Winter"
         }
 
         private List<String> Get_Terms_For_Menu()
@@ -59,20 +54,70 @@ namespace CollegeRegistrationApp.StudentControls
 
             if (DateTime.Now.Month >= 1 && DateTime.Now.Month <= 4)
             {
+                terms.Add("Winter " + currentYear);
+                terms.Add("Spring " + currentYear);
+                terms.Add("Summer " + currentYear);
+                terms.Add("Fall " + currentYear);
+            }
+            else if (DateTime.Now.Month >= 5 && DateTime.Now.Month <= 6)
+            {
+                terms.Add("Spring " + currentYear);
+                terms.Add("Summer " + currentYear);
                 terms.Add("Fall " + currentYear);
                 terms.Add("Winter " + nextYear);
             }
+            else if (DateTime.Now.Month >= 7 && DateTime.Now.Month <= 8)
+            {
+                terms.Add("Summer " + currentYear);
+                terms.Add("Fall " + currentYear);
+                terms.Add("Winter " + nextYear);
+                terms.Add("Spring " + nextYear);
+            }
             else if (DateTime.Now.Month >= 9 && DateTime.Now.Month <= 12)
             {
+                terms.Add("Fall " + currentYear);
                 terms.Add("Winter " + nextYear);
-                terms.Add("Fall " + nextYear);
+                terms.Add("Spring " + nextYear);
+                terms.Add("Summer" + nextYear);
             }
 
             return terms;
 
         }
 
-       
+        private void Term_Click(object sender, EventArgs e)
+        {
+            string term = comboBox1.Text;
+            string[] splitTerm = term.Split(' ');
+
+
+            string query1 = $"execute dbo.getCLasses '{splitTerm[0]}', {splitTerm[1]}";
+
+            MessageBox.Show(query1);
+            SqlDataReader? allClasses = connection.GetDataReader(query1);
+            if (allClasses != null && allClasses.HasRows)
+            {
+                classScheduleTerm.Rows.Clear();
+                while (allClasses.Read())
+                {
+                    classScheduleTerm.Rows.Add(
+                        allClasses["Dept_Name"].ToString(),
+                        allClasses["Course_Name"].ToString(),
+                        allClasses["Section_ID"].ToString(),
+                        allClasses["Day"].ToString(),
+                        allClasses["time_start"].ToString(),
+                        allClasses["time_end"].ToString()
+                        );
+
+
+                    //MessageBox.Show(term);
+                }
+                if (allClasses != null)
+                {
+                    allClasses.Close();
+                }
+            }
+        }
     }
 }
 
