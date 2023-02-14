27b84c10c70dcd63ac41867a89986746a0a0d273
sniezekk <sniezekk@mymacewan.ca>
2023-02-13 22:00:40 -0700

    Enroll works, message is procedure return message

--- CollegeRegistrationApp/CollegeRegistrationApp/SQL/DBConnection.cs
+++ CollegeRegistrationApp/CollegeRegistrationApp/SQL/DBConnection.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace CollegeRegistrationApp.SQL
 {
@@ -66,7 +67,32 @@ namespace CollegeRegistrationApp.SQL
             return rowsAffected;
         }
 
-        // returns the insertion id
+        public String? ExecuteRegisterProcedureWithReturn(
+            String StudentId,
+            String CourseId,
+            String SectionId,
+            String Year,
+            String Semester
+            )
+        {
+            SqlCommand command = new SqlCommand("dbo.registerInClass", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@StudentId", SqlDbType.Int);
+            command.Parameters.Add("@CourseId", SqlDbType.Int);
+            command.Parameters.Add("@SectionId", SqlDbType.Int);
+            command.Parameters.Add("@Year", SqlDbType.Int);
+            command.Parameters.Add("@Semester", SqlDbType.VarChar, 6);
+            command.Parameters.Add("@Message", SqlDbType.VarChar, 50).Direction = ParameterDirection.Output; ;
+
+            command.Parameters["@StudentId"].Value = StudentId;
+            command.Parameters["@CourseId"].Value = CourseId;
+            command.Parameters["@SectionId"].Value = SectionId;
+            command.Parameters["@Year"].Value = Year;
+            command.Parameters["@Semester"].Value = Semester;
+
+            command.ExecuteNonQuery();
+            return command.Parameters["@Message"].Value.ToString();
+        }
 
     }
 }
